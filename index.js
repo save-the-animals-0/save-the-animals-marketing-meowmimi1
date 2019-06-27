a0054593d03849cef631ad2d3079c56ead1ab8b6
Mimi Hoang <miminhoang1@gmail.com>
2019-06-27 17:59:46 -0400

    copyright

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let nav = document.querySelectorAll("nav");
 nav[0].addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = "blue";
+    event.target.style.backgroundColor = "lightblue";
     event.target.style.transition = "3s";
 });
 nav[0].addEventListener("mouseleave", (event) => {
-    event.target.style.backgroundColor = "green";
+    event.target.style.backgroundColor = "lightgreen";
     event.target.style.transition = "3s";
 });
+
+const foot = document.querySelector('footer');
+    foot.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = "lightblue";
+    });

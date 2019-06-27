90bd9bbc0891de9bf7812ad790f9ba5f32f4530a
Mimi Hoang <miminhoang1@gmail.com>
2019-06-27 18:47:19 -0400

    EL done

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ nav[0].addEventListener("mouseleave", (event) => {
     event.target.style.transition = "3s";
 });
 
+let paragraph = document.querySelector('p')
+paragraph.addEventListener('click' , (event) => {
+    event.target.style.color = 'lightgreen';
+});
+
+const h1 = document.querySelectorAll('h1');
+h1[0].addEventListener('click', (event) => event.target.style.color = 'crimson');
+h1[0].addEventListener('click', (event) => event.stopPropagation());
 const foot = document.querySelector('footer');
     foot.addEventListener('mouseover', function(event){
         event.target.style.backgroundColor = "lightblue";
